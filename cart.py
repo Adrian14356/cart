8b189d8d8cfff0463b8f55fd12d85b280a633f78
Adrian <88058915+Adrian14356@users.noreply.github.com>
2021-09-04 02:40:49 +0200

    Add files via upload

---
+++ cart.py
@@ -0,0 +1,2 @@
+products=[{'product_id':134, 'quantity':2, 'price':158, 'free_delivery':True},{'product_id':125, 'quantity':1, 'price':280, 'free_delivery':True}, {'product_id':589, 'quantity':4, 'price':610, 'free_delivery':False}]
+cart={'client_id':'Robert1234', 'data_create':'2021-08-12', 'data_accessed':'2021-08-13'}
